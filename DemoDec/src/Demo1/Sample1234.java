ed26ce37164716da9e05611f377f10f7dd224631
Dhanaraj <Dhanarajvlsidesign@gmail.com>
2018-12-19 13:00:09 +0530

    First-commit

---
+++ DemoDec/src/Demo1/Sample1234.java
@@ -0,0 +1,17 @@
+package Demo1;
+
+public class Sample1234 {
+	
+	 static int a=10;
+	 
+	 final static  String s1="Dhanaraj";
+
+	public static void main(String[] args) {
+		Sample1234.a=20;
+		System.out.println(a);
+	
+	 System.out.println(s1);
+
+	}
+
+}
